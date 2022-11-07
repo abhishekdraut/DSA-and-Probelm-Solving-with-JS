5d97e9a0b3a4dd3e631c71da18b322c848ce147f
Abhishek Dipak Raut <68543904+abhishekdraut@users.noreply.github.com>
2022-11-07 14:59:46 +0530

    Time: 109 ms (17.24%), Space: 42.4 MB (11.66%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    
+    
+    const wordsArray=s.split(" ");
+    for (let i=0;i<wordsArray.length;i++){
+       if(wordsArray[i]==""){
+          wordsArray.splice(i,1)
+          i--
+       }
+    }
+    return wordsArray[wordsArray.length-1].length
+};
