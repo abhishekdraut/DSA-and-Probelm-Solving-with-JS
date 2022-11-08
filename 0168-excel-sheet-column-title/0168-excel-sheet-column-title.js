6a997957fbf7c58dcf4ca0602ff4ee9de1b6fff6
Abhishek Dipak Raut <68543904+abhishekdraut@users.noreply.github.com>
2022-11-08 22:49:01 +0530

    Time: 66 ms (89.57%), Space: 41.7 MB (66.61%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+        
+    let str = '';
+
+    while(columnNumber > 0) {
+        
+        columnNumber--
+        str =String.fromCharCode((columnNumber % 26) + 65)+str;
+        columnNumber = Math.floor(columnNumber / 26);
+    }
+    return str
+};
