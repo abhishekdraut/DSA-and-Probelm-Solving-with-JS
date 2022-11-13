fd177b0481db2e47da546fa8f51b85fbc4bc2ae4
Abhishek Dipak Raut <68543904+abhishekdraut@users.noreply.github.com>
2022-11-13 12:17:34 +0530

    Time: 94 ms (61.94%), Space: 42 MB (56.06%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+  let pointer=0;
+    for(let i=0;i<t.length;i++){
+        if(t[i]==s[pointer]){
+            pointer+=1
+        }
+    }
+   if(pointer==s.length){
+       return true
+   } 
+    else{
+        return false
+    }
+};
