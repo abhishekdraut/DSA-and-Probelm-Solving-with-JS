9c25f49fd84853a2abb657f5f72fc3d758dacb5b
Abhishek Dipak Raut <68543904+abhishekdraut@users.noreply.github.com>
2022-11-13 12:53:17 +0530

    Time: 59 ms (95.89%), Space: 41.7 MB (75.32%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.js
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var countSegments = function(s) {
-let z=s.split(" ");
-let q=[];
-    
-for(let item in z){
-    if(z[item]!=""){
-        q.push(z[item])
-    }
-}
-    return q.length
+return s.split(" ").filter(n => {return n.length > 0}).length;
 };
